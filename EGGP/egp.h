bc2dec71816dbc919efa04f5632f1c82f22910b5
timothyatkinson <tjatkinson95@gmail.com>
2018-04-16 12:01:49 +0100

    Began EGGP

---
+++ EGGP/egp.h
@@ -0,0 +1,31 @@
+#ifndef EGGP_H
+#define EGGP_H
+
+#include <stdint.h>
+#include "../common_functions.h"
+#include "../evolutionary_algorithm.h"
+#include "../generic_operators.h"
+#include "../utils.h"
+
+#include "graph.h"
+#include "common.h"
+
+typedef struct CGP_init_env{
+  Function_Set* fset;
+  GP_Dataset* dataset;
+  int nodes;
+  int max_arity;
+  int pop_size;
+} CGP_init_env;
+
+EGGP_init_env* default_eggp_init_env(GP_Dataset* dataset, Function_Set* fset);
+GP_1_plus_lambda_env* default_eggp_select_env(Function_Set* fset);
+GP_eval_env* default_eggp_eval_env(GP_Dataset* dataset, Function_Set* fset);
+Target_0_env* default_eggp_termination_env();
+EAArgs* default_eggp_EAArgs(GP_Dataset* dataset, Function_Set* fset);
+Fixed_pop_env* default_eggp_pop_size_env();
+
+Graph** eggp_init(uintptr_t env_pointer);
+Graph* eggp_mutate(Graph* host, Function_Set* fset, double mutation_rate);
+
+#endif
